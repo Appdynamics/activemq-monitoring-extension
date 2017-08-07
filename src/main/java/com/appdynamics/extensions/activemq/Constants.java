506394756809bf5bf5dc2e9a74a4013898f7a8f3
Bhuvnesh Kumar <raghur171@gmail.com>
2017-08-07 14:11:01 -0700

    added constants and updated Test file with inital tests for composite tests

---
+++ src/main/java/com/appdynamics/extensions/activemq/Constants.java
@@ -0,0 +1,9 @@
+package com.appdynamics.extensions.activemq;
+
+public class Constants {
+
+    public static final String OBJECT_NAME = "objectName";
+    public static final String METRICS = "metrics";
+    public static final String INCLUDE = "include";
+    public static final String DISPLAY_NAME = "displayName";
+}
