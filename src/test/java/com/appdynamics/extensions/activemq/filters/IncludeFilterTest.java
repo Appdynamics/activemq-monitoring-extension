d0bc04fd17428e23484d02f039692a20ecefaf17
Bhuvnesh Kumar <raghur171@gmail.com>
2018-03-13 11:56:36 -0700

    added license files

--- src/test/java/com/appdynamics/extensions/activemq/filters/IncludeFilterTest.java
+++ src/test/java/com/appdynamics/extensions/activemq/filters/IncludeFilterTest.java
@@ -1,4 +1,12 @@
 
+/*
+ *   Copyright 2018. AppDynamics LLC and its affiliates.
+ *   All Rights Reserved.
+ *   This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *   The copyright notice above does not evidence any actual or intended publication of such source code.
+ *
+ */
+
 package com.appdynamics.extensions.activemq.filters;
 
 import com.appdynamics.extensions.activemq.DictionaryGenerator;
